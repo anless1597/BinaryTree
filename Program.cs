b30c495b1d19bcda93d7eeb8502d2bec377edc20
anless1597 <anna.vesh123@gmail.com>
2023-05-12 17:49:46 +0500

    bfs, empty tree handling, ToString()

--- Program.cs
+++ Program.cs
@@ -5,14 +5,19 @@
         static void Main(string[] args)
         {
             BinaryTree binaryTree = new BinaryTree();
-            int[] nodes = new int[] { 25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90 };
-            //int[] nodes = new int[] { 15, 10, 22, 4, 12, 18, 24, };
-            foreach (int i in nodes)
-            {
-                binaryTree.Add(i);
-            }
+            //int[] nodes = new int[] { 25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90 };
+            // int[] nodes = new int[] { 25, 15, 50, 10, 22, 35, 70, };
+            // //int[] nodes = new int[] { 15, 10, 22, 4, 12, 18, 24, };
+            // foreach (int i in nodes)
+            // {
+            //     binaryTree.Add(i);
+            // }
             Console.WriteLine("start print");
-            binaryTree.Print();
+            Console.WriteLine(binaryTree);
+            binaryTree.Inorder();
+            binaryTree.Preorder();
+            binaryTree.Postorder();
+            binaryTree.BFS();
             Console.WriteLine("end");
         }
     }
